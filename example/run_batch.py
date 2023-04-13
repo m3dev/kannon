6da144147d07140ceb3ee2497572a07f33a225b1
maronuu <oguri.ytr.2000@gmail.com>
2023-04-13 22:19:02 +0900

    Format

--- example/run_batch.py
+++ example/run_batch.py
@@ -1,12 +1,11 @@
 """ This script runs on master job. """
 import logging
 
-import luigi
-from kubernetes import config, client
 import fire
-
+import luigi
 # import task definition
 import tasks
+from kubernetes import client, config
 
 from kannon import Kannon
 
@@ -16,23 +15,23 @@ logging.basicConfig(level=logging.INFO)
 def _create_task_instances():
     task_a = tasks.TaskA(param="a")
     task_b = tasks.TaskB(param="b", parent=task_a)
-    
+
     task_c0 = tasks.TaskC(param="c0", parent=task_b)
     task_c1 = tasks.TaskC(param="c1", parent=task_b)
     task_c2 = tasks.TaskC(param="c2", parent=task_b)
-    
+
     task_d0 = tasks.TaskD(param="d0", parent=task_c0)
     task_d1 = tasks.TaskD(param="d1", parent=task_c1)
     task_d2 = tasks.TaskD(param="d2", parent=task_c2)
-    
+
     task_e0 = tasks.TaskE(param="e0", parent=task_d0)
     task_e1 = tasks.TaskE(param="e1", parent=task_d1)
     task_e2 = tasks.TaskE(param="e2", parent=task_d2)
-    
+
     task_f = tasks.TaskF(param="f", parent_0=task_e0, parent_1=task_e1, parent_2=task_e2)
-    
+
     task_g = tasks.TaskG(param="g", parent=task_f)
-    
+
     return task_g
 
 
@@ -44,36 +43,17 @@ def _get_template_job_object():
             name="kannon-child",
             namespace="default",
         ),
-        spec=client.V1JobSpec(
-            template=client.V1PodTemplateSpec(
-                spec=client.V1PodSpec(
-                    service_account_name="job-manager",
-                    containers=[
-                        client.V1Container(
-                            name="job",
-                            image="kannon_quick_starter",
-                            image_pull_policy="IfNotPresent",
-                            volume_mounts=[
-                                client.V1VolumeMount(
-                                    name="cache-volume",
-                                    mount_path="/cache"
-                                )
-                            ]
-                        )
-                    ],
-                    restart_policy="Never",
-                    volumes=[
-                        client.V1Volume(
-                            name="cache-volume",
-                            persistent_volume_claim=client.V1PersistentVolumeClaimVolumeSource(
-                                claim_name="cache-volume-claim"
-                            )
-                        )
-                    ]
-                )
-            )
-        )
-    )
+        spec=client.V1JobSpec(template=client.V1PodTemplateSpec(spec=client.V1PodSpec(
+            service_account_name="job-manager",
+            containers=[
+                client.V1Container(name="job",
+                                   image="kannon_quick_starter",
+                                   image_pull_policy="IfNotPresent",
+                                   volume_mounts=[client.V1VolumeMount(name="cache-volume", mount_path="/cache")])
+            ],
+            restart_policy="Never",
+            volumes=[client.V1Volume(name="cache-volume", persistent_volume_claim=client.V1PersistentVolumeClaimVolumeSource(
+                claim_name="cache-volume-claim"))]))))
 
 
 def main():
@@ -85,7 +65,7 @@ def main():
     v1 = client.BatchV1Api()
     # create task instance
     task_root = _create_task_instances()
-    
+
     # create template job object
     template_job = _get_template_job_object()
     # run master job
