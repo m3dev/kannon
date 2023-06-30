559e39818cf2afeef90f3b86ca3373fb9b599fa4
yutaro-oguri <yutaro-oguri@m3.com>
2023-06-30 14:42:44 +0900

    Make magic number constant

--- kannon/kube_util.py
+++ kannon/kube_util.py
@@ -13,6 +13,10 @@ class JobStatus(enum.Enum):
     SUCCEEDED = 1
     FAILED = 2
 
+# Max length of job name is 63.
+# https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names
+JOB_NAME_MAX_LENGTH = 63
+
 
 def create_job(api_instance: client.BatchV1Api, job: client.V1Job, namespace: str) -> None:
     api_response = api_instance.create_namespaced_job(
@@ -32,8 +36,8 @@ def get_job_status(api_instance: client.BatchV1Api, job_name: str, namespace: st
 
 def gen_job_name(job_prefix: str) -> str:
     job_suffix = f"{datetime.now().strftime('%Y%m%d%H%M%S')}-{str(random.randint(0, 255)).zfill(3)}"
-    job_prefix = job_prefix[:62 - len(job_suffix)]
+    job_prefix = job_prefix[:JOB_NAME_MAX_LENGTH - 1 - len(job_suffix)]
     job_name = f"{job_prefix}-{job_suffix}"
     job_name = job_name.replace("_", "-").lower()
-    job_name = job_name[:63]
+    job_name = job_name[:JOB_NAME_MAX_LENGTH]
     return job_name
